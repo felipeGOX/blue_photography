19a50f97c674b999a783167181a9c971186f6e60
felipeGOX <luisfelipe.lfgo59@gmail.com>
2023-05-08 19:06:37 -0400

    adicion de migracion rol usuario

--- database/migrations/2023_05_08_225334_create_rol_usuarios_table.php
+++ database/migrations/2023_05_08_225334_create_rol_usuarios_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('rol_usuarios', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->unsignedBigInteger('rol_id');
+          //  $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
+           // $table->foreign('rol_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamps();
         });
     }
