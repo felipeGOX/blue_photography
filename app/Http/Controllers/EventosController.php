282900b2e8c7ac7b1c05386d2d8ed600b70f32d4
felipeGOX <luisfelipe.lfgo59@gmail.com>
2023-05-09 09:40:27 -0400

    Eventos

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Eventos;
 use Illuminate\Http\Request;
 
 class EventosController extends Controller
 {
     //
+    public function index()
+    {
+        $Eventos = Eventos::all(); //saco todos los paquetes de la  tabla paquetes(base de datos)
+        return view('Paquetes.index_adminlte', compact('Eventos')); //retorno a la vista y envio una variable con todos los paquetes
+    }
+    public function create()
+    {
+        return view('Eventos.create');
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update(Request $request, $id)
+    {
+    }
+    public function destroy($id)
+    {
+    }
 }
