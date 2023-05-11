c1c52f996145a8aac8d0618db0b4901afcd86b49
felipeGOX <luisfelipe.lfgo59@gmail.com>
2023-05-11 00:58:29 -0400

    ultimo cambio

--- database/migrations/2023_05_05_013123_create_eventos_table.php
+++ database/migrations/2023_05_05_013123_create_eventos_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->date('fecha');
             $table->time('hora');
             $table->unsignedBigInteger('id_catalogo')->nullable();
+            $table->unsignedBigInteger('id_organizador')->nullable();
           
             $table->timestamps();
         });
